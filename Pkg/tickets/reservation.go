47a29efbc551dad6ec3e2aeb549c1b938f7ef825
Ambarish563 <edimadakalaveeraveal@udayton.edu>
2023-04-26 17:24:43 -0400

    Intial_commit

---
+++ Pkg/tickets/reservation.go
@@ -0,0 +1,118 @@
+package tickets
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type Reservation struct {
+	mtx   sync.Mutex
+	seats []bool
+}
+
+// NewReservation(numSeats int) *Reservation: a constructor function that creates a new Reservation\n
+// object with a seats field of length numSeats (all initially set to false)
+func NewReservation(numSeats int) *Reservation {
+	return &Reservation{seats: make([]bool, numSeats)}
+}
+
+// CheckAvailability(numSeats int) bool: a method that checks if numSeats seats are available for reservation.
+// It returns true if available, false otherwise.
+func (r *Reservation) CheckAvailability(numSeats int) bool {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	if numSeats > len(r.seats) {
+		return false
+	}
+	for i := 0; i < len(r.seats); i++ {
+		if !r.seats[i] {
+			available := true
+			for j := 1; j < numSeats && i+j < len(r.seats); j++ {
+				if r.seats[i+j] {
+					available = false
+					i += j
+					break
+				}
+			}
+			if available {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// ReserveSeats(numSeats int) ([]int, error): a method that reserves numSeats seats if available,\n
+// and returns their seat numbers as a slice of integers. If the seats are not available, it returns nil and an error.
+func (r *Reservation) ReserveSeats(numSeats int) ([]int, error) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	if numSeats > len(r.seats) {
+		return nil, errors.New("not enough seats available")
+	}
+	var seats []int
+	for i := 0; i <= len(r.seats)-numSeats; i++ {
+		if !r.seats[i] {
+			available := true
+			for j := 1; j < numSeats; j++ {
+				if r.seats[i+j] {
+					available = false
+					i += j
+					break
+				}
+			}
+			if available {
+				for j := 0; j < numSeats; j++ {
+					r.seats[i+j] = true
+					seats = append(seats, i+j+1)
+				}
+				return seats, nil
+			}
+		}
+	}
+	return nil, errors.New("not enough seats available")
+}
+
+// CancelReservation is a method of the Reservation struct. It takes a slice of seat numbers as input and returns an error.
+// It cancels the reservation of the seats in the slice by setting their corresponding values in the seats slice to false.
+// If any seat number in the slice is invalid or not reserved, the method returns an error.
+func (r *Reservation) CancelReservation(seats []int) error {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	for _, seat := range seats {
+		if seat < 1 || seat > len(r.seats) {
+			return fmt.Errorf("invalid seat number: %d", seat)
+		}
+		if !r.seats[seat-1] {
+			return fmt.Errorf("seat %d is not reserved", seat)
+		}
+		r.seats[seat-1] = false
+	}
+	return nil
+}
+
+type Confirmation struct {
+	seats []int
+}
+
+// NewConfirmation(seats []int) *Confirmation: a constructor function that creates a new Confirmation object with a seats field set to seats.
+func NewConfirmation(seats []int) *Confirmation {
+	return &Confirmation{seats: seats}
+}
+
+// GenerateConfirmationCode() string: a method that generates a random confirmation code and returns it as a string.
+func (c *Confirmation) GenerateConfirmationCode() string {
+	codeBytes := make([]byte, 4)
+	if _, err := rand.Read(codeBytes); err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(codeBytes)
+}
+
+// PrintConfirmationDetails(): a method that prints the details of the reservation, including the seats reserved and the confirmation code.
+func (c *Confirmation) PrintConfirmationDetails() {
+	fmt.Printf("Seats reserved: %v\nConfirmation code: %s\n", c.seats, c.GenerateConfirmationCode())
+}
---
+++ Pkg/tickets/reservation_test.go
@@ -0,0 +1,83 @@
+package tickets_test
+
+import (
+	"io/ioutil"
+	ticket "myproject/tickets"
+	"os"
+	"testing"
+)
+
+func TestReservation_CheckAvailability(t *testing.T) {
+	r := ticket.NewReservation(10)
+	if !r.CheckAvailability(5) {
+		t.Errorf("expected true, got false")
+	}
+	if r.CheckAvailability(11) {
+		t.Errorf("expected false, got true")
+	}
+}
+
+func TestReservation_ReserveSeats(t *testing.T) {
+	r := ticket.NewReservation(10)
+	seats, err := r.ReserveSeats(5)
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+	if len(seats) != 5 {
+		t.Errorf("expected 5 seats reserved, got %d", len(seats))
+	}
+	seats, err = r.ReserveSeats(6)
+	if err == nil {
+		t.Errorf("expected error but we got no error")
+	}
+	if seats != nil {
+		t.Errorf("expected nil seats, got %v", seats)
+	}
+}
+
+func TestReservation_CancelReservation(t *testing.T) {
+	r := ticket.NewReservation(10)
+	seats, _ := r.ReserveSeats(5)
+	err := r.CancelReservation(seats)
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+	err = r.CancelReservation([]int{11})
+	if err == nil {
+		t.Errorf("expected non-nil error, got nil")
+	}
+	err = r.CancelReservation([]int{1, 3, 5})
+	if err == nil {
+		t.Errorf("expected non-nil error, got nil")
+	}
+}
+
+func TestConfirmation_GenerateConfirmationCode(t *testing.T) {
+	c := ticket.NewConfirmation([]int{1, 2, 3, 4, 5})
+	code := c.GenerateConfirmationCode()
+	if code == "" {
+		t.Errorf("expected non-empty code, got empty string")
+	}
+	if len(code) != 8 {
+		t.Errorf("expected 8-character code, got %d characters", len(code))
+	}
+}
+
+func TestConfirmation_PrintConfirmationDetails(t *testing.T) {
+	c := ticket.NewConfirmation([]int{1, 2, 3})
+
+	// Capture output from PrintConfirmationDetails
+	oldOutput := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	c.PrintConfirmationDetails()
+
+	w.Close()
+	os.Stdout = oldOutput
+
+	outputBytes, _ := ioutil.ReadAll(r)
+	if len(string(outputBytes)) == 0 {
+		t.Errorf("It is not Printing the Confirmation Details ")
+	}
+}
